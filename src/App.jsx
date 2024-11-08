5b8aaa67b41df1f6f4f3d9325a8fe14171dedf16
bagaskara2134 <111202113829@mhs.dinus.ac.id>
2024-11-08 09:10:13 +0700

    coba di lokal baru

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hezkia Bagaskara</h1>
+      <h2>Testt hehe</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
